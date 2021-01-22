1221791a2510a89b23ba3f514074d70e64320d68
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-23 01:12:34 +0300

    Generate User model

---
+++ db/migrate/20210122221157_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :user_name
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps
+    end
+  end
+end
