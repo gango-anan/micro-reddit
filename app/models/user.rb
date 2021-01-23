ae9d6d5c3ddbff528bb4504bf2c72115b89d1569
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-23 22:05:55 +0300

    add Post model and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :user_name, :email, :password, presence: true
   validates :user_name, :email, uniqueness: true
+  has_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_22_221157) do
+ActiveRecord::Schema.define(version: 2021_01_23_185420) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "user_name"
