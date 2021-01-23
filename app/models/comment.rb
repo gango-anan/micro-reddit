96412569cb95a09ffb2b298ed77069dc21d0c01a
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-23 23:24:16 +0300

    update Comment model validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
-  validates :title, :content, :user_id, :post_id, presence true
-  validates :title, uniqueness true
+  validates :content, :user_id, :post_id, presence: true
+  validates :post_id, uniqueness: true
+  validates :content, length: { in: 2..102}
   belongs_to :user
   belongs_to :post
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :user_name, :email, :password, presence: true
   validates :user_name, uniqueness: true
   has_many :posts
+  has_many :comments
 end
