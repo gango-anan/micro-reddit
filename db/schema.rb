404660338bc3da2b5db706161a71690a9f7d5686
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-23 20:10:52 +0300

    migrate db to create users table in the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_01_22_221157) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
