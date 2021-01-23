59ae9c8c2fdca8b2b6c3d2047965f44c436dc44e
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-23 23:02:43 +0300

    migrate db to create the comments table in the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_23_185420) do
+ActiveRecord::Schema.define(version: 2021_01_23_195121) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
