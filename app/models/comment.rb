6fd36b06be8fa7223f9b8aa7b4c87c1e25ee2060
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-23 23:05:43 +0300

    update the Comment model with user_id and post_id validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validates :title, :content, presence true
+  validates :title, :content, :user_id, :post_id, presence true
   validates :title, uniqueness true
   belongs_to :user
   belongs_to :post
