fdca86e7ad56be9df4ce42ce851e5ac358f764f0
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-23 23:01:34 +0300

    add validation and associations to the Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+  validates :title, :content, presence true
+  validates :title, uniqueness true
+  belongs_to :user
+  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates :title, uniqueness: true
   validates :content, length: { in: 5..105}
   belongs_to :user
+  has_many :comments
 end
