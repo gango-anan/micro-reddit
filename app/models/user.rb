d5bb6072fc5421287b2d68b8fa53adf104a41328
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-23 22:47:42 +0300

    add association to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :user_name, :email, :password, presence: true
-  validates :user_name, :email, uniqueness: true
+  validates :user_name, uniqueness: true
   has_many :posts
 end
